e68cc39588fe329452042c855ffae840a367994b
carlton <64127681+notcarlton@users.noreply.github.com>
2023-06-15 11:47:21 -0600

    basic struffs

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include "common.h"
+
+void entry() {
+    AllocConsole();
+    AttachConsole(GetCurrentProcessId());
+    SetConsoleTitleA("DirectX Playground");
+
+    FILE* in;
+    freopen_s(&in, "CONIN$", "r", stdin);
+    FILE* out;
+    freopen_s(&out, "CONOUT$", "w", stdout);
+    FILE* err;
+    freopen_s(&err, "CONOUT$", "w", stderr);
+    
+    printf("Injected.\n");
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        std::thread entryThread(entry);
+        entryThread.detach();
+    }
+
+    return TRUE;
+}
